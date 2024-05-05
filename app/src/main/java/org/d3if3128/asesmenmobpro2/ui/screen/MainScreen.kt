2944de7e0a2e8925ba373bfb9eb4aa69308bb9dd
Syfanadya Wening Adi <syfanadya5@gmail.com>
2024-05-05 15:59:10 +0700

    Membuat tampilan list

--- app/src/main/java/org/d3if3128/asesmenmobpro2/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro2/ui/screen/MainScreen.kt
@@ -1,9 +1,14 @@
 package org.d3if3128.asesmenmobpro2.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
@@ -13,9 +18,13 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.d3if3128.asesmenmobpro2.R
+import org.d3if3128.asesmenmobpro2.model.Peminjaman
 import org.d3if3128.asesmenmobpro2.ui.theme.AsesmenMobpro2Theme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -40,14 +49,36 @@ fun MainScreen(){
 }
 @Composable
 fun ScreenContent(modifier: Modifier){
-    Column (
-        modifier
-            .fillMaxSize()
-            .padding(16.dp)
+    val viewModel: MainViewModel = viewModel()
+    val data = viewModel.data
+    LazyColumn (
+        modifier = modifier.fillMaxSize()
     ){
-
+        items(data){
+            ListItem(peminjaman = it)
+            Divider()
+        }
     }
 }
+
+@Composable
+fun ListItem(peminjaman: Peminjaman){
+    Column (
+        modifier = Modifier.fillMaxWidth().padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        Text(
+            text = peminjaman.judulbuku,
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis,
+            fontWeight = FontWeight.Bold
+        )
+        Text(text = peminjaman.status)
+        Text(text = peminjaman.tanggalpinjam)
+        Text(text = peminjaman.tanggalkembali)
+    }
+}
+
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
 @Composable
--- app/src/main/java/org/d3if3128/asesmenmobpro2/ui/screen/MainViewModel.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro2/ui/screen/MainViewModel.kt
@@ -8,15 +8,15 @@ class MainViewModel: ViewModel(){
     private fun getDataDummy(): List<Peminjaman>{
         val data = mutableListOf(
             Peminjaman(1, "Syfanadya Wening Adi", "6706223128", "081358008183",
-                "Harry Potter 2020", "Sedang dipinjam","2024-02-15 12:34:56", "2024-02-15 12:34:56"),
+                "Harry Potter 2020", "Sedang dipinjam","2024-02-15 12:34:56", "2024-02-22 12:34:56"),
             Peminjaman(2, "Chaeza Fauzyyah Inayah", "6706223056", "082344678900",
-                "Dilan & Milea", "Sedang dipinjam","2024-02-16 11:34:56", "2024-02-16 11:34:56"),
+                "Dilan & Milea", "Sedang dipinjam","2024-02-16 11:34:56", "2024-02-23 11:34:56"),
             Peminjaman(3, "Feby Irmayana", "6706220079", "082444678900",
-                "The Design Thinking Playbook", "Sedang dipinjam","2024-02-17 10:34:56", "2024-02-17 10:34:56"),
+                "The Design Thinking Playbook", "Sedang dipinjam","2024-02-17 10:34:56", "2024-02-24 10:34:56"),
             Peminjaman(4, "Dyna Rosalina Pangaribuan", "6706223016", "082444679900",
-                "Ensiklopedia Sains", "Sedang dipinjam","2024-02-18 10:34:56", "2024-02-18 10:34:56"),
+                "Ensiklopedia Sains", "Sedang dipinjam","2024-02-18 10:34:56", "2024-02-25 10:34:56"),
             Peminjaman(5, "Mariana", "6706220079", "082444678900",
-                "Finding Yourself in the Age of Distractions", "Sedang dipinjam","2024-02-19 10:34:56", "2024-02-19 10:34:56"),
+                "Finding Yourself in the Age of Distractions", "Sedang dipinjam","2024-02-19 10:34:56", "2024-02-26 10:34:56"),
         )
         return data
     }
