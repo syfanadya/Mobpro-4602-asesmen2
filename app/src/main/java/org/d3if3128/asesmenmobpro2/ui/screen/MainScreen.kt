29ca7e376bee9295318f8f0736e010289d81851c
Syfanadya Wening Adi <syfanadya5@gmail.com>
2024-05-06 00:46:05 +0700

    Menambahkan gambar saat empty state / data kosong

--- app/src/main/java/org/d3if3128/asesmenmobpro2/ui/screen/MainScreen.kt
+++ app/src/main/java/org/d3if3128/asesmenmobpro2/ui/screen/MainScreen.kt
@@ -2,12 +2,16 @@ package org.d3if3128.asesmenmobpro2.ui.screen
 
 import android.content.res.Configuration
 import android.widget.Toast
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
@@ -21,6 +25,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -58,10 +63,18 @@ fun ScreenContent(modifier: Modifier){
     val context = LocalContext.current
     if (data.isEmpty()){
         Column(
-            modifier = modifier.fillMaxSize().padding(16.dp),
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.emptynote),
+                contentDescription = "Empty data image",
+                modifier = Modifier.size(200.dp)
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             Text(text = stringResource(id = R.string.list_kosong))
         }
     }
